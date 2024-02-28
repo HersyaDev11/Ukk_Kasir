2eee987a06cb71dc6c5074b489e1d4a0bad9e049
HersyaDev11 <145486775+HersyaDev11@users.noreply.github.com>
2024-02-28 09:07:57 +0700

    Add files via upload

---
+++ db_kasir_ci4.sql
@@ -0,0 +1,239 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 11 Feb 2024 pada 16.15
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kasir_ci4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_jual`
+--
+
+CREATE TABLE `tbl_jual` (
+  `id_jual` int(11) NOT NULL,
+  `no_faktur` varchar(15) DEFAULT NULL,
+  `tgl_jual` date DEFAULT NULL,
+  `jam` time DEFAULT NULL,
+  `grand_total` int(11) DEFAULT NULL,
+  `dibayar` int(11) DEFAULT NULL,
+  `kembalian` int(11) DEFAULT NULL,
+  `id_kasir` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'Makanan'),
+(2, 'Minuman'),
+(3, 'Bumbu Masakan'),
+(4, 'Elektronik'),
+(5, 'Alat Tulis Kantor'),
+(6, 'Aksesoris');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_produk`
+--
+
+CREATE TABLE `tbl_produk` (
+  `id_produk` int(11) NOT NULL,
+  `kode_produk` varchar(25) DEFAULT NULL,
+  `nama_produk` varchar(150) DEFAULT NULL,
+  `id_kategori` int(25) DEFAULT NULL,
+  `id_satuan` int(25) DEFAULT NULL,
+  `harga_beli` int(11) DEFAULT NULL,
+  `harga_jual` int(11) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_produk`
+--
+
+INSERT INTO `tbl_produk` (`id_produk`, `kode_produk`, `nama_produk`, `id_kategori`, `id_satuan`, `harga_beli`, `harga_jual`, `stok`) VALUES
+(1, '11111', 'Permen Kis', 1, 7, 5000, 7000, 50),
+(2, '11112', 'Frisian Flag Kental Manis', 2, 10, 8000, 10000, 50),
+(3, '11113', 'Aqua', 2, 15, 6000, 8000, 50),
+(5, '11114', 'Coca Cola', 2, 15, 10000, 15000, 60);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_rinci`
+--
+
+CREATE TABLE `tbl_rinci` (
+  `id_rinci` int(11) NOT NULL,
+  `no_faktur` varchar(15) DEFAULT NULL,
+  `kode_produk` varchar(25) DEFAULT NULL,
+  `harga_jual` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `total_harga` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_satuan`
+--
+
+CREATE TABLE `tbl_satuan` (
+  `id_satuan` int(2) NOT NULL,
+  `nama_satuan` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_satuan`
+--
+
+INSERT INTO `tbl_satuan` (`id_satuan`, `nama_satuan`) VALUES
+(1, 'PCS'),
+(2, 'BOX'),
+(3, 'Lusin'),
+(4, 'Buah'),
+(5, 'KG'),
+(6, 'Unit'),
+(7, 'Bungkus'),
+(9, 'Karung'),
+(10, 'Kaleng'),
+(15, 'Liter');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(2) NOT NULL,
+  `nama_user` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `level` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `email`, `password`, `level`) VALUES
+(1, 'dea', 'admin@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 1),
+(2, 'agam', 'agam@gmail.com', '\n7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 2),
+(3, 'Zakiiii', 'Zakia123@gmail.com', 'cf9dcdb74ac6eeb803c1b4e769a84291ab7e3828', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_jual`
+--
+ALTER TABLE `tbl_jual`
+  ADD PRIMARY KEY (`id_jual`);
+
+--
+-- Indeks untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indeks untuk tabel `tbl_rinci`
+--
+ALTER TABLE `tbl_rinci`
+  ADD PRIMARY KEY (`id_rinci`);
+
+--
+-- Indeks untuk tabel `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  ADD PRIMARY KEY (`id_satuan`);
+
+--
+-- Indeks untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_jual`
+--
+ALTER TABLE `tbl_jual`
+  MODIFY `id_jual` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_rinci`
+--
+ALTER TABLE `tbl_rinci`
+  MODIFY `id_rinci` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  MODIFY `id_satuan` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
